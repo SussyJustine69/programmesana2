78ef84a700c4502d24533d9a30da1e87c490dbc7
SussyJustine69 <mazinamazins@inbox.lv>
2023-09-11 13:48:12 +0300

    ddd

--- klases.js
+++ klases.js
@@ -1,10 +1,50 @@
 class  Cilveks {
- constructor (age, name, sex){
+ constructor (age = 0, name = "Anna", sex = "V"){
     this.vecums = age;
     this.vards = name;
     this.dzimums = sex;
-
+    this.info();
+   
  }
+svinetDzimeni(){
+   this.vecums++;
+   this.info()
+}
+mainitVardu(jaunaisVards){
+   this.vards = jaunaisVards;
+
 
 }
+mainitDzimumu(jaunaisDzimums ="hz"){
+if (jaunaisDzimums == "" ) {
+ if( this.dzimums == "S"){
+     this.dzimums = "V";
+ } else{
+this.dzimums = "S";
+
+ }
+ 
+}
+ else {
+     this.dzimums = jaunaisDzimums;
+ }
+
+ 
+}
+info(){
+  let teksts = "Sveiki, mani sauc" + this.vards + "Man ir " + this.vecums + "gadu."
+  teksts += "Es esmu "
+   if (this.dzimums == "S") {
+       teksts += "sieviete."
+   } else if (this.dzimums == "V"){
+ teksts += "vīrietis."
+
+   } else  { teksts += this.dzimums}
+   
+console.log(teksts)
+}
+}
+
+
  pirmais = new Cilveks(15, "Katrīna", "S")
+
